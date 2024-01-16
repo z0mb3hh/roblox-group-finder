74bf080a003401f0b2105da6b85162b46c773d34
z0mb3hh <156729850+z0mb3hh@users.noreply.github.com>
2024-01-15 17:06:47 -0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ async function findOpenGroups() {
     try {
       const group = await axios.get(`https://groups.roproxy.com/v1/groups/${i}`);
       if (group.data.publicEntryAllowed === true && group.data.owner === null) {
-        await axios.post('YOUR_WEBHOOK_URL', {
+        await axios.post('https://discord.com/api/webhooks/1196615108844068864/RYqACJpB28mBod7eN6Hqw9is0GBPZJwp7RBFkS8xfzBEipfKGVh7Q4F40aJtHaCwpcB5', {
           content: `Found an open group: https://www.roblox.com/groups/${i}`
         });
       }
